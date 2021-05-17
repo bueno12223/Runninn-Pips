ee30268da009750412c1266a770e94eedb101a6b
Jesus <berriojesus122@gmail.com>
2021-05-17 14:09:43 -0500

    media end

--- src/frontend/components/landing/suscribe.jsx
+++ src/frontend/components/landing/suscribe.jsx
@@ -3,7 +3,7 @@ import './styles/suscribe.scss'
 function suscribe () {
   return (
     <section className='suscribe'>
-      <h2>Suscribete a nuestro news letter</h2>
+      <h2>Ingresa tu datos y te contactaremos</h2>
       <p>Suscribete para obtener la última información</p>
       <input type='email' placeholder='email' />
       <input type='text' placeholder='nombre' />

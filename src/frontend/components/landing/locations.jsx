87583775a1e220f3b14e3943b90a43318bf689fc
Jesus <berriojesus122@gmail.com>
2021-05-17 10:08:12 -0500

    put locations

---
+++ src/frontend/components/landing/locations.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function locations () {
+  return (
+    <section>
+      <img src='https://laslesvpn-landing.netlify.app/images/locations.svg' alt='' />
+
+    </section>
+  )
+}
+
+export default locations

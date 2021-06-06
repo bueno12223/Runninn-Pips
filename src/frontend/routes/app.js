e24f3634a51539b648dc0d7abb8020546e463b41
Jesus Berrio <berriojesus122@gmail.com>
2021-06-05 20:17:48 -0400

    conflicto app arreglado

--- src/frontend/routes/app.js
+++ src/frontend/routes/app.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Landing from '../pages/landing'
 import Home from '../pages/home'
-<<<<<<< HEAD
 import Login from '../pages/login'
 import registro from '../pages/registro'
 import Reproductor from '../pages/reproductor'
@@ -17,20 +16,8 @@ const App = () => (
         <Route exact path='/home/:id' render={(props) => <Reproductor {...props} />} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/registro' component={registro} />
-
       </Switch>
     </Layout>
-=======
-import Header from '../components/general/header'
-import '../assets/styles/app.scss'
-const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route exact path='/' component={Landing} />
-      <Route exact path='/home' component={Home} />
-    </Switch>
->>>>>>> ea0a54b5b6ebb5990601a5360fbaaa0306d2af36
   </BrowserRouter>
 )
 

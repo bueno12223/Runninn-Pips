ac9a4b1153b213a3ebc5499cda18c1cd3d9cf540
Jesus Berrio <berriojesus122@gmail.com>
2021-06-05 20:21:12 -0400

    conflicto header arreglado

--- src/frontend/components/general/header.jsx
+++ src/frontend/components/general/header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import logo from '../../assets/images/logo-s.jpg'
 import { Link } from 'react-router-dom'
@@ -22,29 +21,6 @@ function header () {
         <Link to='/registro' className='lheader-links__button' onClick={() => setDisplay(!display)}>Registrate</Link>
       </div>
     </header>
-=======
-import React from 'react'
-import logo from '../../assets/images/logo-s.jpg'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-function header () {
-  return (
-    <Navbar className='bg-light justify-content-between'>
-      <Navbar.Brand>
-        <img
-          width={133}
-          height={80}
-          className='mr-3'
-          src={logo}
-          alt='Generic placeholder'
-        />
-      </Navbar.Brand>
-      <Nav className='mr-2'>
-        <Nav.Link href='#home'>Iniciar Sesión</Nav.Link>
-        <Nav.Link href='#home'>Registrarme</Nav.Link>
-      </Nav>
-    </Navbar>
->>>>>>> ea0a54b5b6ebb5990601a5360fbaaa0306d2af36
   )
 }
 
--- src/frontend/pages/home.jsx
+++ src/frontend/pages/home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import CardList from '../components/home/cardList'
 import Hero from '../components/home/hero'
 function home () {
@@ -8,12 +7,6 @@ function home () {
       <Hero />
       <CardList />
     </>
-=======
-
-function home () {
-  return (
-    <div />
->>>>>>> ea0a54b5b6ebb5990601a5360fbaaa0306d2af36
   )
 }
 

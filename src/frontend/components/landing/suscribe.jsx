dbf30c876b9e28d9daeb37a576038a9642ef34eb
Jesus Berrio <berriojesus122@gmail.com>
2021-06-12 18:05:50 -0400

    informacion del landing nuevamente actualizada

--- src/frontend/components/landing/suscribe.jsx
+++ src/frontend/components/landing/suscribe.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './styles/suscribe.scss'
 function suscribe () {
   return (
-    <section className='suscribe'>
-      <h2>Ingresa tu datos y te contactaremos</h2>
-      <p>Suscribete para obtener la última información</p>
-      <input type='email' placeholder='email' />
-      <input type='text' placeholder='nombre' />
-      <input type='number' placeholder='numero de contacto' />
-      <a href='/'>Suscribirse</a>
-    </section>
+    <>
+      <section className='suscribe'>
+        <Link className='lhero-button' to='/registro'>Registarme ahora!!</Link>
+        <h2>¿Aún tienes duda?</h2>
+        <p>LLena estos datos y te contactaremos!</p>
+        <input type='email' placeholder='email' />
+        <input type='text' placeholder='nombre' />
+        <input type='number' placeholder='numero de contacto' />
+        <a href='/'>Suscribirse</a>
+      </section>
+    </>
   )
 }
 
--- src/frontend/routes/app.js
+++ src/frontend/routes/app.js
@@ -6,6 +6,7 @@ import Login from '../pages/login'
 import registro from '../pages/registro'
 import Reproductor from '../pages/reproductor'
 import Layout from '../components/general/layout'
+import notFound from '../pages/404'
 import '../assets/styles/app.scss'
 const App = ({ isLogged }) => (
   <BrowserRouter>
@@ -23,6 +24,7 @@ const App = ({ isLogged }) => (
         />
         <Route exact path='/login' component={Login} />
         <Route exact path='/registro' component={registro} />
+        <Route component={notFound} />
       </Switch>
     </Layout>
   </BrowserRouter>

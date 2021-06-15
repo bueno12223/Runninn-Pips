492ad225c3986208cc0c83c36b98b60f4a2e3377
Jesus Berrio <berriojesus122@gmail.com>
2021-06-15 14:53:18 -0400

    transacction routes añadida

---
+++ src/server/routes/transacctions.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+const transacctions = (app) => {
+  app.post('/transaction', async (req, res) => {
+    const { userID, url, userName } = req.body
+    try {
+      const result = await axios({
+        method: 'POST',
+        data: { userID, url, userName },
+        url: `${process.env.API_URL}/transaction`
+      })
+      res.status(201).json({ data: result.data.result })
+    } catch (e) {
+      console.log(e)
+    }
+  })
+}
+export default transacctions

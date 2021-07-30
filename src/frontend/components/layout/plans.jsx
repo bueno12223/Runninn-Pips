27e196ce18b662de0d775a6cd9639e5ebd64f2a0
Jesus Berrio <berriojesus122@gmail.com>
2021-07-29 21:00:41 -0500

    ahora es 1

--- src/frontend/components/layout/plans.jsx
+++ src/frontend/components/layout/plans.jsx
@@ -18,7 +18,7 @@ function plans ({ to = '/registro', onClick = () => {} }) {
             'Mentorías personalizada'
           ]}
           isCheck={check}
-          price={140}
+          price={1}
           tile='Plan inversor inteligente'
           to={to}
           onClick={onClick}

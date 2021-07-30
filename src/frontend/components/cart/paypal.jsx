b0eaa5b37de0c7c018ee7b50f6eea7e1ccd8130b
Jesus Berrio <berriojesus122@gmail.com>
2021-07-29 20:56:32 -0500

    bug de paypal 5

--- src/frontend/components/cart/paypal.jsx
+++ src/frontend/components/cart/paypal.jsx
@@ -1,37 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { PayPalButton } from 'react-paypal-button-v2'
 import { messageHandler, validatePayment } from '../../actions'
 function PayPalBtn (props) {
-  const paypal = useRef()
-  useEffect(async () => {
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions, err) => {
-          return actions.order.create({
-            intent: 'CAPTURE',
-            purchase_units: [
-              {
-                description: `Accesso a la plataforma de Running Trader con el plan de ${props.amount}`,
-                amount: {
-                  currency_code: 'USD',
-                  value: 1.00
-                }
-              }
-            ]
-          })
-        },
-        onError: (err) => {
-          console.error(err)
-          props.messageHandler({ message: 'Ocurrio un error, intente mas tarde', success: false })
-        }
-      })
-      .render(paypal.current)
-  }, [])
-
   return (
     <PayPalButton
-      amount='1.00'
+      amount={props.amount}
       onSuccess={async (details, data) => {
         window.alert('Transaction completed by ' + details.payer.name.given_name)
         await props.validatePayment({ id: data.orderID })

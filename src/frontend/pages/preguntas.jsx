71b279e1f82897be6fcee668cf6c8ffbea28fa1d
Jesus Berrio <berriojesus122@gmail.com>
2021-08-02 16:44:08 -0500

    preguntas añadidas

---
+++ src/frontend/pages/preguntas.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+
+function preguntas () {
+  return (
+    <>
+      <h1 className='title'>¿Cuánto es el máximo de afiliados que uno puede tener?</h1>
+      <p className='text mb'>No hay límite de afiliados, puedes tener todo cuanto puedas.
+
+      </p>
+      <h2 className='title'>¿Que son Divisas?
+
+      </h2>
+      <p className='text mb'>Son medios monetarios y de valor representando (no solo billetes de curso legal extranjeros) en un mercado internacional.
+
+      </p>
+
+      <h2 className='title'>¿Qué es un Par en Forex?
+
+      </h2>
+      <p className='text mb'>Un par de divisas son las dos divisas utilizadas para operar en Forex (Operación comercial de compra-venta). En otras palabras es dar el valor de una unidad de una divisa relativo al valor de otra divisa en el mercado internacional de divisas Forex.
+
+      </p>
+
+      <h2 className='title'>¿Qué es forex y cómo funciona?
+
+      </h2>
+      <p className='text mb'>Forex (Foreing Exchange), tambien conocido como FX, Mercado de Divisas o trading de Divisas, se trata de un mercado mundial descentralizado de las diferentes Divisas que operan en todo el mundo sin lugar a dudas es el mercado más importante. Forex se centra en el cambio de una moneda por otra. En este mercado los pares están presentes y se encuentran relacionados por corredores (Brokers) que facilitan a los titulares de las cuentas la ejecución de cambios. Lo que se pretende con los cambios de una moneda a otra es conservar la operación hasta que se produzca un aumento de valor, para volver a cambiar Y así conseguir un beneficio.
+
+      </p>
+
+      <h2 className='title'>¿ Cuál es la diferencia del trading enseñado en RunningTrader al trading que enseñan otras academias?
+
+      </h2>
+      <p className='text mb'>Runningtrader es una academia que enseña el análisis institucional (análisis de profesionales market maker) aplicado no solo a Forex, también al mercado de Acciones, Materias, Índices, Criptomonedas. En nuestro programa educativo no se contemplan la educación que redunda en medios como Youtube, Brokers, Exchange, entre otros ya que una de nuestras misiones es que el estudiante aprenda como realmente se realizan las operaciones profesionales del 5% de traders que se quedan con el dinero del 95% de trader que aprenden lo básico.
+
+      </p>
+
+      <h2 className='title'>¿Qué sistema tienen para no quemar las cuentas?
+
+      </h2>
+      <p className='text mb'>En Runninig trader te enseñaremos a trabajar apegado a un plan de trading, junto al análisis institucional para que  arriesgues máximo el 1,5% en cada invercion.
+
+      </p>
+
+      <h2 className='title'>¿Puedo ver trading en vivo en la academia Runningtrading?
+
+      </h2>
+      <p className='text mb'>Contamos con clases en vivo todos los días en diferentes horarios que se pueden abordar a tus necesidades.
+
+      </p>
+
+      <h2 className='title'>¿Debo dejar mi trabajo para ser trader?
+
+      </h2>
+      <p className='text mb'>El trading puede ser una actividad complementaria a tus ingresos económicos, con responsabilidad, no recomendamos dejar un empleo hasta lograr la consistencia en el trading y tener los ingresos necesarios para sustentarse económicamente.
+      </p>
+      <h2 className='title'>¿No tengo experiencia puedo ser trader?
+
+      </h2>
+      <p className='text mb'>En RunningTrader te guiaremos desde lo más básico para que aprendas desde cero a ser un trader con éxito, consistencia y rentable. Desde una mirada al trading con responsabilidad.
+
+      </p>
+
+      <h2 className='title'>¿Garantizan el éxito de sus estudiantes?
+
+      </h2>
+      <p className='text mb'>No le garantizamos; lo que sí te podemos garantizar es que nuestro programa de entrenamiento en trading institucional es uno de los mejores y con información avanzada, herramientas para mentorías privadas con los profesores.
+
+      </p>
+
+      <h2 className='title'>¿Que necesito para empezar en el trading?
+
+      </h2>
+      <p className='text mb'>En Runningtrader te guiaremos para que al comenzar a operar en el mercado tengas un plan de gestión de riesgo, sepas cómo evaluar a los brokers confiables, deberás contar con una computadora o celular inteligente, conexión a internet y un acompañamiento en su proceso de aprendizaje.
+
+      </p>
+      <h2 className='title'>¿Necesito tener conocimientos empresariales, en economía, en finanzas o en matemáticas avanzadas para aprender trading?
+
+      </h2>
+      <p className='text mb'>No se requiere ningún conocimiento o formación previa dado que el trading es una actividad completamente diferente, que requiere tanto conocimiento técnico, así como un autoconocimiento propio. El programa educativo en Runningtrader te llevará desde los aspectos más básicos del trading institucional a los más avanzados con una visión práctica y que puedas comprender con todos los medios posibles para lograrlo.
+
+      </p>
+      <h2 className='title'>¿Puedo ver a los profesores operando en vivo?
+
+      </h2>
+      <p className='text mb'>Si, puedes ver clases en vivo con nuestros mentores, en los que puedes evaluar el mercado y también interactuar con los mentores para que te aclaren dudas al respecto de la clase.
+
+      </p>
+      <h2 className='title'>¿De no poder estar en la clase en vivo como puedo hacer para ver la información?
+
+      </h2>
+      <p className='text mb'>Las clases en vivo quedan grabadas en nuestra plataforma de www.Runingtrader.com, en el canal dispuesto para el mentor de dicha clase. Por lo cual podrás ver la información así no hayas podido estar en vivo.
+
+      </p>
+      <h2 className='title'>¿Lo que aprendo sólo funcionará para forex?
+
+      </h2>
+      <p className='text mb'>No, la información que tenemos en nuestro programa educativo en Runningtrader, una vez aprendida podrás aplicarlo y/o adaptarlo en el análisis de mercado de forex, criptomonedas, acciones y futuros.
+
+      </p>
+      <h2 className='title'>¿Cuándo comenzaré a ver ganancias una vez que empiece a estudiar en su Academia?
+
+      </h2>
+      <p className='text mb'>Eso dependerá de ti, de tu esfuerzo, constancia y disciplina a la hora de operar siguiendo el programa de trading y de gestión de riesgo, en runningtrader tenemos una visión responsable del trading con nuestros alumnos donde primeramente te guiaremos para que operes en una cuenta demo de tu broker, para luego con confianza operes en los mercados reales.
+
+      </p>
+      <h2 className='title'>¿Existe algún cobro adicional por las memorias privadas que tiene la Academia?
+
+      </h2>
+      <p className='text mb'>No, las mentorías privadas están incluidas una vez que accedes a la academia y no tienen límites de mentorías, puedes programar cuantas vayas necesitando.
+
+      </p>
+    </>
+  )
+}
+
+export default preguntas
